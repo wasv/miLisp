88c25467e469100ffdf37be47cee20658d220ff1
Billy Stevens <contact@wasv.me>
2021-07-27 19:20:40 -0400

    Fixes bug on EOF with empty buffer.

--- src/main.c
+++ src/main.c
@@ -5,18 +5,24 @@
 #define MAX_LINE 1024
 
 int main(int argc, char **argv) {
-    milisp_err_t err = 0;
+    char *line = NULL;
+    ssize_t line_len = 0;
+    size_t line_buf_len = 0;
+    char outbuf[MAX_LINE];
 
-    while(err == 0) {
-        char *line = NULL;
-        size_t line_len = 0;
-        char outbuf[MAX_LINE];
-
+    uint8_t exit = 0;
+    while(!exit) {
         printf("user> ");
-        getline(&line, &line_len, stdin);
-        err = milisp_rep(line, outbuf, MAX_LINE);
-        printf("%s", outbuf);
+        line_len = getline(&line, &line_buf_len, stdin);
+        if(line_len >= 0) {
+            milisp_rep(line, outbuf, MAX_LINE);
+            printf("%s", outbuf);
+        } else {
+            printf("\n");
+            exit = 1;
+        }
     }
+    free(line);
 
     return 0;
 }
--- src/mal/step0_repl/mal.c
+++ src/mal/step0_repl/mal.c
@@ -3,10 +3,7 @@
 #include <string.h>
 
 milisp_err_t milisp_rep(char *source, char *result_buf, size_t result_buf_len) {
-    if(strlen(source) == 0) {
-        memset(result_buf, 0, result_buf_len);
-        return 1;
-    }
+    memset(result_buf, 0, result_buf_len);
     strncpy(result_buf, source, result_buf_len);
     return 0;
 }
