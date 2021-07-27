67c1a4bb4879e744dcdbdfe534cb4d0fb5350ffb
Billy Stevens <contact@wasv.me>
2021-07-27 19:31:07 -0400

    Clang format.

--- src/main.c
+++ src/main.c
@@ -5,24 +5,24 @@
 #define MAX_LINE 1024
 
 int main(int argc, char **argv) {
-    char *line = NULL;
-    ssize_t line_len = 0;
-    size_t line_buf_len = 0;
-    char outbuf[MAX_LINE];
+  char *line = NULL;
+  ssize_t line_len = 0;
+  size_t line_buf_len = 0;
+  char outbuf[MAX_LINE];
 
-    uint8_t exit = 0;
-    while(!exit) {
-        printf("user> ");
-        line_len = getline(&line, &line_buf_len, stdin);
-        if(line_len >= 0) {
-            milisp_rep(line, outbuf, MAX_LINE);
-            printf("%s", outbuf);
-        } else {
-            printf("\n");
-            exit = 1;
-        }
+  uint8_t exit = 0;
+  while (!exit) {
+    printf("user> ");
+    line_len = getline(&line, &line_buf_len, stdin);
+    if (line_len >= 0) {
+      milisp_rep(line, outbuf, MAX_LINE);
+      printf("%s", outbuf);
+    } else {
+      printf("\n");
+      exit = 1;
     }
-    free(line);
+  }
+  free(line);
 
-    return 0;
+  return 0;
 }
